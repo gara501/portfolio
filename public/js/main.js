72fd8b092c0839377465e7888b26af1815cdacd5
gara501 <gandres.ramirez@gmail.com>
2019-03-05 23:53:44 -0500

    Portfolio 1.0

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,78 @@
 const app = new Vue({
   el: '#terminal-app',
   data: {
-    line1: false,
+    info: false,
+    show: '',
+    showskills: '',
+    error: false,
+    counter: 5,
     commands: [
       'help',
       'whois',
-      'info',
-      'skills'
+      'projects',
+      'skills',
+      'clear'
     ]
+  },
+  methods: {
+    expand: function(e) {
+      const terminal = document.querySelector('.terminal');
+      if (terminal.classList.contains('expanded')) {
+        terminal.classList.remove('expanded');
+      } else {
+        terminal.classList.add('expanded');
+      }
+      
+    },
+    ghost: function(e) {
+      const ghostdiv = document.querySelector('.ghost');
+      ghostdiv.classList.add('active');
+      setTimeout(()=> {
+        ghostdiv.classList.remove('active');
+      }, 100)
+    },
+    close: function(e) {
+      const bye = document.querySelector('.bye');
+      bye.classList.add('active');
+      setInterval(()=> {
+        this.counter--;
+        if (this.counter === 0) {
+          window.location.href = "https://www.linkedin.com/in/gonzaloandresramirez/";
+        }
+      }, 1000)
+    },
+    validateConsole: function(e) {
+      const code = e.target.value;
+      if (code.length > 0) {
+        this.error = false;
+        if (e.keyCode === 13) {
+          switch (code) {
+            case 'help':
+              this.info = true; 
+              break;
+            case 'whois':
+              this.show = 'show';
+              break;
+            case 'code':
+              
+              break;
+            case 'skills':
+              this.showskills = 'show';
+              break;
+            case 'clear':
+              this.info = false;
+              this.error = false;
+              this.show = '';
+              break;
+            default:
+              this.error = true;
+              break;
+          }
+          e.target.value = '';
+        }         
+      }
+      
+      this.log += e.key;
+    }
   }
 })
